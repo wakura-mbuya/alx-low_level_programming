85fcb344347363fc4814308ad8ca7d262c114997
wakura-mbuya <wakura9@gmail.com>
2023-05-24 02:47:32 +0300

    fix-0x01. C - Variables, if, else, while-Task 1-Remove mixed declaration and code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,27 +11,26 @@
  */
 int main(void)
 {
-	int n, last_digit;
+	int n, k; /* k is last digit of n */
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
-	if (last_digit > 5)
+	k = n % 10;
+	if (k > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, k);
 	}
 	else
 	{
-		if (last_digit == 0)
+		if (k == 0)
 		{
-			printf("Last digit of %d is %d and is 0\n", n, last_digit);
-		}
+			printf("Last digit of %d is %d and is 0\n", n, k);
+		}i
 		else
 		{
-			if (last_digit < 6)
+			if (k < 6)
 			{
-			printf("Last digit of %d is %d and"
-				       " is less than 6 and not 0\n", n, last_digit);
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, k);
 			}
 		}
 	}
