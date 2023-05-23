b567b6a054b31d466bdd91d8f1cf67a419356818
wakura-mbuya <wakura9@gmail.com>
2023-05-24 01:45:24 +0300

    0x01. C - Variables, if, else, while-Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Assigns a random number to a variable n
+ * each time its executed
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAiX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else
+	{
+		if (n == 0)
+		{
+			printf("%d is zero\n", n);
+		}
+		else
+		{
+			printif("%d is negative\n", n);
+		}
+	return (0);
+}
