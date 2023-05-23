6ddb2ffaf95f25b3314e26e7a5476201391c641e
wakura-mbuya <wakura9@gmail.com>
2023-05-23 04:47:27 +0300

    0x00. C - Hello, World - Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ * followed by a new line  on standard output using puts built-in C function
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	return (puts("Programming is like building a multilingual puzzle"));
+}
