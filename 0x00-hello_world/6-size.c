231b49cd0fba0034ef7a6dd123d18a39b8b6341c
wakura-mbuya <wakura9@gmail.com>
2023-05-23 05:37:54 +0300

    0x00. C - Hello, World - Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Description: Prints the sizes of various types
+ * on the computer the program is compiled and run on
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(unsigned long));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+
+	return (0);
+}
