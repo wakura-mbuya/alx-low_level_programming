8ba044dc2cbcb43e173b976b936c31de21fc788f
wakura-mbuya <wakura9@gmail.com>
2023-05-23 05:14:41 +0300

    0x00. C - Hello, World - Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Description: Prints "with proper grammmar, but the
+ * outcome is a piece of art" followed by a new line
+ * on standard output using printf built-in C function
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
