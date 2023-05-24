44d2d38e83d17dedc97736b46bfd3d6bbb913df3
wakura-mbuya <wakura9@gmail.com>
2023-05-24 04:30:43 +0300

    0x02. C - Functions, nested loops-Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <std.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "_putchar" on standard output
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+void main()
+{
+	puts("_putchar");
+	return (0);
+}
