596730e9010393b3ea2598b60e8927a909861450
wakura-mbuya <wakura9@gmail.com>
2023-05-24 07:16:20 +0300

    0x01. C - Variables, if, else, while - Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints lowercase alphabet in reverse
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= a; c--)
+	{
+		putchar(c);
+	}
+	putchar(10);
+
+	return (0);
+}
