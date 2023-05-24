1a3629bab439646e4d42aedf3f2fb43041a30321
wakura-mbuya <wakura9@gmail.com>
2023-05-24 06:57:54 +0300

    0x01. C - Variables, if, else, while - Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints digits 0 to 9 and a new line
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf('\n');
+}
