c66292cec8291ec3b3147cc103bebaff9269bafc
wakura-mbuya <wakura9@gmail.com>
2023-05-24 05:56:26 +0300

    0x02. C - Functions, nested loops-Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * _islower - Checks for lowercase character
+ * @c: The character to be checked
+ *
+ * Return: 1 if lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	char k; /* for iterating the alphabets */
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		if (k == c)
+			return (1);
+	}
+	return (0);
+}
