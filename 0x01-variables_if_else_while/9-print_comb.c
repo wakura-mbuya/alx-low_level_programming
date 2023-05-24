af68c5980a7895cb69ebb863fc0c052848c6b167
wakura-mbuya <wakura9@gmail.com>
2023-05-24 07:38:13 +0300

    0x01. C - Variables, if, else, while - Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints digits 0 to 9
+ * Digits are separated by a comma (',')
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 57; n++)
+	{
+		putchar(n);
+		putchar(44);
+	}
+	putchar(57);
+	putchar(10);
+
+	return (0);
+}
