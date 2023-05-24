eec7f3d7672ecea812974e84d7b5af3f2e85cc5a
wakura-mbuya <wakura9@gmail.com>
2023-05-24 06:30:52 +0300

    0x01. C - Variables, if, else, while - Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c; /* for iterating through the alphabet */
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
