e4d4045a35152c4f64923ceffa0f47b305c94fec
wakura-mbuya <wakura9@gmail.com>
2023-05-24 06:48:00 +0300

    0x01. C - Variables, if, else, while - Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <std.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase
+ * Except letters q and e
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'q' || c == 'e')
+			continue;
+		putchar(c);
+	}
+	putchar('\n);
+
+	return (0);
+}
