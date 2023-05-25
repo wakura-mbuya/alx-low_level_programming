f33158bd45a6bfc8a7ed4634d8b95b70ea1a4820
wakura-mbuya <wakura9@gmail.com>
2023-05-25 04:51:10 +0300

    fix-0x02. C - Functions, nested loops - Task 8 - fix output

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,11 +13,11 @@ void jack_bauer(void)
 			for (k = 0; k < 6; k++)
 				for (l = 0; k < 10; k++)
 				{
-					_putchar(i);
-					_putchar(j);
+					_putchar(i + '0');
+					_putchar(j + '0');
 					_putchar(':');
-					_putchar(k);
-					_putchar(l);
+					_putchar(k + '0');
+					_putchar(l + '0');
 					_putchar('\n');
 				}
 }
