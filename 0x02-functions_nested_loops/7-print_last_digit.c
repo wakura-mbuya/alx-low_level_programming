fbe24874cc95ea93216d7f63626dc8159e4e243b
wakura-mbuya <wakura9@gmail.com>
2023-05-25 03:16:35 +0300

    0x02. C - Functions, nested loops - Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @num: The number whose last digit is to be printed
+ *
+ * Return: The last digit of num
+ */
+int print_last_digit(int num)
+{
+	return (num % 10);
+}
