d1aed12750f5682e2e2cfccaa508913abf644a81
wakura-mbuya <wakura9@gmail.com>
2023-05-25 06:01:09 +0300

    0x02. C - Functions, nested loops - Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: the first number to be printed
+ */
+void print_to_98(int n)
+{
+	int a;
+
+	if (n < 98)
+	{
+		for (n = 0; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d", 98);
+	}
+	else
+	{
+		for (a = n; a > 98; a--)
+			printf("%d, ", a);
+		printf("%d", 98);
+	}
+}
