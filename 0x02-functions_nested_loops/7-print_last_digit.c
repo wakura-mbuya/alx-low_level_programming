666fb823153e3a84e4254a1ad895a5e200f7549d
wakura-mbuya <wakura9@gmail.com>
2023-05-25 04:20:49 +0300

    fix-0x02. C - Functions, nested loops - Task 7-add stdio.h header file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,10 @@ int print_last_digit(int num)
 {
 	int last_digit;
 
-	last_digit = num % 10;
-	_putchar(last_digit + '0');
-
 	if (num < 0)
-		return (last_digit* -1);
+		last_digit = (num % 10) * -1;
 	else
-		return (last_digit);
+		last_digit = num % 10;
+	_putchar((last_digit % 10) + '0');
+	return (last_digit % 10);
 }
