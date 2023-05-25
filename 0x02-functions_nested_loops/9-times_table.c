0ac5a2ee57a271f2fafa8e0d1bfb559b64138390
wakura-mbuya <wakura9@gmail.com>
2023-05-25 05:23:34 +0300

    0x02. C - Functions, nested loops - Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,6 +8,7 @@ void times_table(void)
 {
 	int i, j, count;
 
+	count = 1;
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
@@ -20,6 +21,7 @@ void times_table(void)
 			}
 			else
 				_putchar('\n');
+			count++;
 		}
 		_putchar('\n');
 	}
