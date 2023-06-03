de2165be7daec3df41231dfbebb9b1cc122eed5b
wakura-mbuya <wakura9@gmail.com>
2023-06-03 11:19:08 +0300

    0x01. C - Variables, if, else, while - Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j;
+
+	for (i = 0; i < 9; ++i)
+	{
+		for (j = i + 1 ; j < 10; ++j)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+
+			if (i != 8)
+			{
+				putchar(44);
+				putchar(32);
+			}
+
+		}
+	}
+	putchar(10);
+}
