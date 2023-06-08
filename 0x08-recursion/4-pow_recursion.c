d69d8339fcb1084c5c2f4578edf2e79c58c24849
wakura-mbuya <wakura9@gmail.com>
2023-06-08 05:04:11 +0300

    0x08. C - Recursion - Task 4 solution

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Returns the value of x raised to the power of y
+ * @x: The first integer (the base)
+ * @y: The index of y
+ *
+ * Description: Returns the value of x raised to the power of y
+ * if y is lower than 0, the function returns -1
+ * Return: x raised to power y(x ^ y)
+ */
+int _pow_recursion(int x, int y)
+{
+	int n = x;
+
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	if (y <= 1)
+		return (n);
+
+	n *= x;
+	y--;
+	return (_pow_recursion(x, y - 1));
+}
