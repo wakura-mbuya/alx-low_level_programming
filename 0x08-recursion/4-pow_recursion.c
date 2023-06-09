99051a8487894eff518331dd5af54b977a4693f7
wakura-mbuya <wakura9@gmail.com>
2023-06-09 04:57:30 +0300

    fix-0x08. C - Recursion - fix the recursive fuction

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,8 +11,6 @@
  */
 int _pow_recursion(int x, int y)
 {
-	int n = x;
-
 	if (y < 0)
 		return (-1);
 
@@ -20,9 +18,6 @@ int _pow_recursion(int x, int y)
 		return (1);
 
 	if (y <= 1)
-		return (n);
-
-	n *= x;
-	y--;
+		return (x);
 	return (_pow_recursion(x, y - 1));
 }
