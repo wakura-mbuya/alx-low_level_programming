8bd5c2decc7f9b7e3821cef1221db1c32de795eb
wakura-mbuya <wakura9@gmail.com>
2023-06-14 04:11:36 +0300

    0x04. C - More functions and nested loops - Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ * Return: nothing
+ */
+
+print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; ++i)
+	{
+		_putchar(i + '\0');
+	}
+
+	_putchar('\n');
+}
