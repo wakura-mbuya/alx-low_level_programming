030b7cec19cde9082db4162b1f1b4bc2af29a8e9
wakura-mbuya <wakura9@gmail.com>
2023-07-16 09:41:44 +0300

    0x02. C Functions and nested loops - Task 14 (Printing Fibonacci numbers)

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ *
+ * Description: Prints the first 50 Fibonnaci numbers
+ * starting with 1 and 2, followed by a new line
+ * The numbers are separated by a comma, followed by a space
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	long num1, fib, num2;
+	int count;
+
+	num1 = 1;
+	num2 = 2;
+
+	printf("%ld, %ld,", num1, num2);
+	count = 2;
+
+	while (count <= 50)
+	{
+		fib = num1 + num2;
+		num1 = num2;
+		num2 = fib;
+		printf("%ld", fib);
+		count++;
+
+		if (count < 50)
+			printf(", ");
+	}
+
+	printf("\n");
+}
