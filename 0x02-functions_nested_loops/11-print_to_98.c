8407ef780b9c9de4669885641207103f1705ec0d
wakura-mbuya <wakura9@gmail.com>
2023-07-16 07:08:19 +0300

    Fix -0x02. C Functions and nested loops - Task 11 solution (Print natural numbers n to 98) - fix printing of , and space characters

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -25,7 +25,12 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+
+			if (i != 98)
+			{
+				printf(", ");
+			}
 		}
 		_putchar('\n');
 	}
@@ -33,7 +38,12 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+
+			if (i != 98)
+			{
+				printf(", ");
+			}
 		}
 		_putchar('\n');
 	}
