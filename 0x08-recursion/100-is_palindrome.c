40b15abefbfe65638dcc1ab3d4a58404049c502f
wakura-mbuya <wakura9@gmail.com>
2023-07-17 15:52:44 +0300

    fix 0x08 C Recursion - Task 8  (Fix betty coding style error)

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,10 +7,10 @@
  */
 int _strlen_recursion(char *s)
 {
-        if (*s == '\0')
-                return (0);
-        else
-                return (1 + _strlen_recursion(s + 1));
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
 
 /**
@@ -22,13 +22,13 @@ int _strlen_recursion(char *s)
  */
 int comparator(char *s, int n1, int n2)
 {
-        if (*(s + n1) == *(s + n2))
-        {
-                if (n1 == n2 || n1 == n2 + 1)
-                        return (1);
-                return (0 + comparator(s, n1 + 1, n2 - 1));
-        }
-        return (0);
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
+	return (0);
 }
 
 /**
@@ -38,7 +38,7 @@ int comparator(char *s, int n1, int n2)
  */
 int is_palindrome(char *s)
 {
-        if (*s == '\0')
-                return (1);
-        return (comparator(s, 0, _strlen_recursion(s) - 1));
+	if (*s == '\0')
+		return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
