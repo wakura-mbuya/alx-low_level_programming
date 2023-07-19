33d1ae9d2c017800b40f88483acb7f808d4957ad
wakura-mbuya <wakura9@gmail.com>
2023-07-20 02:00:57 +0300

    0x0F C Function Pointers - Task 0 (Function that uses function pointers to print a name)

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ * @name: the name of the person
+ * @f: the function that prints the name
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef _MY_HEADERS_
+#define _MY_HEADERS_
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif
