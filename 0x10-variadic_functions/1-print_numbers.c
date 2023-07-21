c80feafe85e59643c683244a07d31a14eb34813a
wakura-mbuya <wakura9@gmail.com>
2023-07-21 05:53:10 +0300

    0x10 C Variadic functions - task 1 (function to print numbers)

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Prints numbers
+ * @separator: string to separate the numbers
+ * @n: number of integers passed to the function
+ *
+ * Return: Nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int k;
+
+	va_start(valist, n);
+
+	for (k = 0; k < n; i++)
+	{
+		printf("%d", va_arg(valist, int));
+		if (separator && k < n - 1)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(valist);
+}
