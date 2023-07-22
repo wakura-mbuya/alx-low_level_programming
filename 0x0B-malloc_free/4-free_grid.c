21f6b90f7188d86061c19132a833bb3c98fda969
wakura-mbuya <wakura9@gmail.com>
2023-07-22 09:30:06 +0300

    0x0B C Malloc, Free - Task 4(Function to clear 2D array of integers)

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2-D grid
+ * @grid: 2D array of integers
+ * @height: the height of the grid
+ * Return: Nothing
+ */
+void free_grid(int **grid, int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
+}
