a8bedf43e997b15d53ed8483f48f3af88fd95bf3
wakura-mbuya <wakura9@gmail.com>
2023-07-22 07:22:50 +0300

    0x0B C Malloc, Free - Task 2(Function to concatenate 2 strings)

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * str_concat - cancatenates 2 strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to the new string or NULL if
+ * concatenation fails
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, len1, len2, j;
+	char *str_;
+
+	if (s1 == NULL)
+		len1 = 0;
+	else
+	{
+		for (i = 0; s1[i] != NULL; i++)
+			;
+		len1 = i;
+	}
+
+	if (s2 == NULL)
+		len2 = 0;
+	else
+	{
+		for (i = 0; s2[i] != NULL; i++)
+			;
+		len2 = i + 1;
+	}
+
+	str_ = malloc((len1 + len2) * sizeof(char));
+	if (str_ == NULL)
+		return (NULL);
+	for (i = 0; i <= len1; i++)
+		str_[i] = s1[i];
+	for (j = 0; j <= len2; j++)
+		str_[i + j] = s2[j];
+	str_[i + j] = '\0';
+
+	return (str_);
+}
