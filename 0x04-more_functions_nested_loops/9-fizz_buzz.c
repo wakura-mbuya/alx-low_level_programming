fff0c0e5e9f3b12072cf06e05316dc07fe4de3f7
Kennedy Wakura <wakura9@gmail.com>
2023-08-02 04:14:02 +0300

    0x04 C More functions and nested loops - Task 9 (The FizzBuzz puzzle)

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,53 +1,28 @@
 #include <stdio.h>
 
 /**
- * fizzbuzz - prints the Fizz-Buzz test
+ * main - entry point to the program
  *
- * Description: Prints the Fizz-Buzz test
- * The function prints the numbers from 1 to 100, followed by a new line
- * For multiples of 3, the function prints Fizz instead of the number
- * For multiples of 5, the function prints 5 instead of the number
- * For numbers which are multiples of both  and 5, the function prints FizzBuzz
- *
- * Return: nothing
+ * Description: Prints the Fizz-Buzz test puzzle
+ * Return: 0 (success)
  */
-
-void fizzbuzz(void)
+int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; ++i)
+	for (i = 1; i < 101; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-		{
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz");
-		}
 		else if (i % 5 == 0)
-		{
 			printf("Buzz");
-		}
 		else
-		{
 			printf("%d", i);
-		}
-
 		if (i != 100)
-			putchar(' ');
+			printf(" ");
 	}
-	putchar('\n');
-}
-
-/**
- * main - The entry point to the program
- * Return: 0 (Success)
- */
-
-int main(void)
-{
-	fizzbuzz();
+	printf("\n");
 	return (0);
 }
