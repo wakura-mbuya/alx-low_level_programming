53822a0dcca323db4eeead27eb24a22ed182c234
Kennedy Wakura <wakura9@gmail.com>
2023-08-02 03:57:34 +0300

    0x04 C More functions and nested loops Task 8(Function to print squares using #)

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,7 +3,7 @@
 /**
  * print_square - print a square, followed by a new line
  * @size: The size of the square to be printed
- * 
+ *
  * Description: Prints a square followed by a new line
  * Only uses the _putchar function to print
  * Where size is the size of the square
@@ -15,16 +15,15 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (size <= 0)
+	if (size < 1)
 		_putchar('\n');
 	else
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < size; ++j)
-			{
+			for (j = 0; j < size; j++)
 				_putchar('#');
-			}
-			_putchar('#');
+			_putchar('\n');
 		}
 	}
+}
