c4897dcc36f86f8e9f05515d9dbf6b5c74bdccfa
wakura-mbuya <wakura9@gmail.com>
2023-09-05 20:21:08 +0300

    0x05 C. Pointers, arrays and strings - Task 0 solution

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value pointed to by n to 98
+ * @n: Pointer to the variable whose value is to be updated
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
