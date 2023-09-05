95d0ebfa497264c40281b6d5c37e560b6d2a1feb
wakura-mbuya <wakura9@gmail.com>
2023-09-05 20:37:59 +0300

    0x05 C Pointers, Arrays and Strings - Task 1 Solution

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two input integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int temp_;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
