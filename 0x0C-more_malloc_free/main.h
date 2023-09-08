9d276689bd889072216d98c6b292c0fc9f740be0
wakura-mbuya <wakura9@gmail.com>
2023-09-08 05:27:00 +0300

    C More Malloc, Free - Task 0 correction

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,4 +2,4 @@ void *malloc_checked(unsigned int);
 char *string_nconcat(char, char, unsigned int);
 void *_calloc(unsigned int, unsigned int);
 int *array_range(int, int);
-void *_realloc(void, unsigned int, unsigned int);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
