29221d6f2d54c9ba61d36502250a152823359940
wakura-mbuya <wakura9@gmail.com>
2023-09-12 20:20:50 +0300

    Struct and typedefs - fix task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,12 +13,12 @@ void print_dog(struct dog *d)
 	if (d->name)
 		printf("Name: %s\n", d->name);
 	else
-		printf("Name: nil\n");
+		printf("Name: (ni)l\n");
 
 	printf("Age: %f\n", d->age);
 
 	if (d->owner)
 		printf("Owner: %s\n", d->owner);
 	else
-		printf("Owner: nil\n");
+		printf("Owner: (nil)\n");
 }
