e9ef85014eaafdd2bd5a843fd137a41569475bd4
wakura-mbuya <wakura9@gmail.com>
2023-09-12 20:16:14 +0300

    fix C structs and type defs task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,11 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog dog_;
-
-	dog_.name = name;
-	dog_.age = age;
-	dog_.owner = owner;
-
-	d = &dog_;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
