2c92d97c33b233e0c86cae02a5795daf6829e17e
wakura-mbuya <wakura9@gmail.com>
2023-09-12 06:05:41 +0300

    C Structs and typedef - Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MYSTRUCT_H
+#define MYSTRUCT_H
+
+/**
+ * dog struct dog - struct that represents a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Stores information about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
