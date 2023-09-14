605127b74e5c4dd7b0149593ffb3e4b47d200807
wakura-mbuya <wakura9@gmail.com>
2023-09-14 03:11:35 +0300

    Function Pointers - Task 1 correction

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,9 +12,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
-	if (size > 0)
+	if (array && action)
 		for (i = 0; i < size; i++)
 			action(array[i]);
 }
