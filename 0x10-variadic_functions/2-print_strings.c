0ae189e98c97dff0011b70e13ec371502572311d
wakura-mbuya <wakura9@gmail.com>
2023-09-15 06:17:22 +0300

    Variadic functions - Task 2 corrections

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
@@ -10,6 +11,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list args;
+	char *arg;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
