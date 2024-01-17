e757c7dcb322333dcb6c5048daea9a6335a2d009
wakura-mbuya <wakura9@gmail.com>
2024-01-16 18:55:19 -0800

    Malloc - Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - creates a copy of a string
+ * @str: The string to duplicate
+ *
+ * Return: A pointer to a str copy in memory
+ */
+char *_strdup(char *str)
+{
+	unsignedd int length;	/* lenght of str */
+	char *_str;	/* stores a copy of str */
+	int i;	/* for iterating the while loop */
+
+	/* get the length of str */
+	length = 0;
+	while (str[length] != '\0')
+		length++;
+	length++;
+
+	if (str == NULL)
+		return (NULL);	/* returns Null if str is NULL */
+
+	/* Allocate memory */
+	_str = malloc(length);
+	if (_str == NULL)
+		return (NULL);	/* checks if malloc failed */
+
+	for (i = 0; i < length; i++)
+		_str[i] = str[i];
+	return (_str);
+}
