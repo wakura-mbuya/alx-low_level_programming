396ad9f1e2c233375a8506b1ae8f3b1229264572
wakura-mbuya <wakura9@gmail.com>
2024-01-23 06:22:04 +0300

    0x04. C - More functions, more nested loops - Task 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,80 +1,47 @@
-#include <stdbool.h>
-#include <inttypes.h>
-#include <math.h>
 #include <stdio.h>
-#include <stdint.h>
+#include <math.h>
+#include <stdbool.h>
 
-bool is_prime(int64_t);
+bool is_prime(int num);
 
 /**
- * main - Entry point to the program
+ * is_prime - checks if an integer is a prime number
+ * @num: the number being checked
  *
- * Description: This program finds and prints the larges prime factor of
- * the number 612852475143, followed by a new line.
- * Return: Nothing
+ * Return: true if the number is a prime number
+ * returns false otherwise
  */
-int main(void)
+bool is_prime(int num)
 {
-	int64_t prime_fact;
-	int64_t sqrt_integer, num_facts, num = 612852475143;
-	double sqrt_result;
+	int x; /* the square root of x */
+	int i;	/* for loop iterator */
 
-	/* Calculate the square root of the number */
-	sqrt_result = sqrt((double)num);
-
-	/* Convert the result back to int64_t */
-	sqrt_integer = (int64_t)sqrt_result;
-	prime_fact = 1;
-
-	for (num_facts = sqrt_integer; num_facts >= 2; num_facts--)
-	{
-		if (num % num_facts == 0)
-		{
-			if (is_prime(num_facts))
-			{
-				prime_fact = num_facts;
-				break;
-			}
-		}
-	}
-
-	printf("%" PRId64 "\n", prime_fact);
-
-	return (0);
+	x = (int)sqrt(num);
+	for (i = 2; i <= x; i++)
+		if (num % i == 0)
+			return (false);
+	return (true);
 }
 
 /**
- * is_prime - checks if a number is a prime number
- * @num: The number being checked if its prime
+ * main - program entry point
  *
- * Return: true if the num is prime, false otherwise
+ *
+ * Return: 0 on success
  */
-bool is_prime(int64_t num)
+int main(void)
 {
-	int64_t i;
+	long x;
+	long y = 1, i;
 
-	if (num <= 1)
-		return (false); /* Numbers less than or equal to 1 are not prime */
-
-	if (num <= 3)
-		return (true); /* 2 and 3 are prime numbers*/
-
-	if (num % 2 == 0 || num % 3 == 0)
-		return (false); /* Numbers divisible by 2 or 3 are not prime */
-
-	/* Check for prime by testing divisors up to the square root of the number */
-	i = 5;
-
-	while (i * i <= num)
+	x = (long)sqrt(612852475143);
+	for (i = 2; i <= x; i++)
 	{
-		if (num % i == 0 || num % (i + 2) == 0)
-		{
-			return (false);
-		}
-		i += 6; /*
-			 *  Optimize for prime checking by
-			 * skipping multiples of 2 and 3
-			 */
+		if (is_prime(i))
+			if (612852475143 % i == 0)
+				y = i;
 	}
-	return (true);
+
+	printf("%ld\n", y);
+	return (0);
 }
