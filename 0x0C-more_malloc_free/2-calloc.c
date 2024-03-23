f490935155eebd96f9f968c8f48121e4fe8a4ff0
wakura-mbuya <wakura9@gmail.com>
2024-03-23 15:44:49 +0300

    C More Malloc, free - Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nums == NULL)
 		return (NULL); /* malloc failed */
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		nums[i] = 0;
 	return (nums);
 }
