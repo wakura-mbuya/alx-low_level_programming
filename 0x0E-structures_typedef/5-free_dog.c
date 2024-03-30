a05d84d5209bd24c4b6227ec7db3aa7ea5f656a2
wakura-mbuya <wakura9@gmail.com>
2024-03-30 16:47:25 +0300

    C Struct,typedef - Task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: struct dog to be freed
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
