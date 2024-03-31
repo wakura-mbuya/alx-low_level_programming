082e1e70409fe4679f9cf4bb6ffda941691b2330
wakura-mbuya <wakura9@gmail.com>
2024-03-31 10:47:12 +0300

    C Variadic Functions - Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,7 +8,8 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	int i, k, sum = 0;
+	int k, sum = 0;
+	unsigned int i;
 
 	if (n == 0)
 		return (0);
@@ -16,7 +17,7 @@ int sum_them_all(const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		k = va_arg(args, int);
-		sum += num;
+		sum += k;
 	}
 
 	va_end(args);
